670dd5c35ba94e0d8227944a79aaccaf87d09bd1
vojtond <Bastl.vojtech@gmail.com>
2014-11-04 21:37:26 +0100

    pozn

--- parser.c
+++ parser.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+/*struct na token*/
+/*funkce: gettoken, body, tìlo, while atd.*/
+/*rekurzivní postup*/
 int main(){
-
 
 return 1;
 }
