85d99f36225efe0375927fbcd2e9e49dcd0f525a
vojtond <Bastl.vojtech@gmail.com>
2014-11-19 22:16:00 +0100

    prvních 9 pravidel

--- parsektest/main.c
+++ parsektest/main.c
@@ -4,6 +4,7 @@
 #include "scanner.h"
 FILE *soubor;
   SToken *token;
+
 int main()
 {
 
@@ -34,6 +35,7 @@ int main()
         {
             printf("nepovedlo se vytvoøit øetìzec\n");
         }
+          if (TYPE())
     printf ("%i",START());
     free(token);
     fclose(soubor);
@@ -41,26 +43,143 @@ int main()
     return 4;
 }
 int START (){
-    int je;
-
-     get_token(soubor,token);
-
-    printf("\nsd\n");
-    if ((token->charnum == BEGIN) || (token->charnum == VAR) || (token->charnum == FUNCTION)) {
-        if (SLOZ()) {
-            printf("\nsd3\n");
-            strFree(token->stri);
+    get_token(soubor,token);
+    if (1/*(token->type == KEY_BEGIN) || (token->type ==KEY_VAR ) || (token->type == KEY_FUNCTION)*/) {
+      if ((DEK()) && (FUNC()) && (SLOZ())) {
             get_token(soubor,token);
-
             token->type==TP_DOT;
             return 1;
         }
-
     }
-printf("\nfgfg\n");
 return 0;
-
 }
+
+/*<FUNC>		->	eps*/
+/*<FUNC> 		->	function id  (<ARG>) : <TYPE> <FORWARD>*/
+int FUNC (){
+
+   if ((1/*token->type == kbegin*/)){
+
+        return 1;
+
+   }else {
+   if (1/*(token->type==KEY_FUNCTION)*/){
+       get_token(soubor,token);
+       if (token->type==TP_IDENT){
+            get_token(soubor,token);
+            if (token->type==TP_LBRA){
+                get_token(soubor,token);
+                if (ARG()) {
+                    if (token->type==TP_RBRA){
+                        get_token(soubor,token);
+                        if (token->type==TP_COL){
+                            get_token(soubor,token);
+
+                                    return TYPE() && FORWAR();
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+   }
+    return 0;
+}
+
+
+/*<FORWARD> 	->	; <DEK> <SLOZ> ; <FUNC>*/
+/*<FORWARD>	->	forward ; <FUNC>*/
+int FORWAR (){
+    if (token->type==ST_SEM){
+        get_token(soubor,token);
+        if ((DEK())&& (SLOZ)) {
+            if (token->type==ST_SEM){
+                get_token(soubor,token);
+                return FUNC();
+            }
+        }
+    }else{
+        if (1/*token->type==KEY_FORWARD*/){
+            get_token(soubor,token);
+            if (token->type==ST_SEM){
+                get_token(soubor,token);
+                return FUNC;
+            }
+        }
+    }
+return 0;
+}
+
+/*<ARG> 		-> 	eps*/
+/*<ARG> 		-> 	id : <TYPE> <ARGDAL>*/
+
+int ARG (){
+    if (token->type==TP_RBRA){
+        return 1;
+    }else{
+        if (token->type==TP_IDENT){
+            get_token(soubor,token);
+            if (token->type==TP_COL){
+                get_token(soubor,token);
+                return TYPE () && ARGDAL();
+            }
+        }
+    }
+}
+
+/*<ARGDAL> 	-> 	; id : <TYPE> <ARGDAL>*/
+/*<ARGDAL> 	-> 	eps*/
+int ARGDAL (){
+     if (token->type==TP_RBRA){
+        return 1;
+    }else{
+        if (token->type==ST_SEM){
+            get_token(soubor,token);
+            if (token-> type==TP_IDENT){
+                get_token(soubor,token);
+                if (token->type==TP_COL){
+                    get_token(soubor,token);
+                    return TYPE() && ARGDAL();
+                }
+            }
+        }
+    }
+}
+
+/*<CYKLUS>	->	while <VYRAZ> do  <SLOZ>   */
+int CYKLUS (){
+return 0;
+}
+
+/*<KDYZ>		->  	if <VYRAZ> then  <SLOZ> <ELSE> */
+int KDYZ (){
+return 0;
+}
+
+/*<ELSE>		->  	else  <SLOZ>*/
+/*<ELSE>		-> 	eps*/
+int ELSEP (){
+return 0;
+}
+
+/*<SEKVENCE>	-> 	<POKYN> <SEKVENCE>*/
+/*<SEKVENCE>	->	eps*/
+int SEKVENCE (){
+return 0;
+}
+
+/*<POKYN>		->	<CYKLUS>*/
+/*<POKYN>		->	<KDYZ>*/
+/*<POKYN>		->	<PRIKAZ>*/
+/*<POKYN>		->	READLN(id) */
+/*<POKYN>		->	WRITE( <VYPIS>)	*/
+/*<POKYN>		->	<SLOZ> */
+int POKYN (){
+return 0;
+}
+
+/*<SLOZ>		->	begin	<PRVNI> end*/
 int SLOZ (){
 
     printf("\nsd3\n");
@@ -78,3 +197,66 @@ int SLOZ (){
      }
 return 0;
 }
+
+/*<PRVNI>		->	eps*/
+/*<PRVNI>		-> 	<POKYN> <DALSI>*/
+int PRVNI (){
+return 0;
+}
+
+/*<DALSI>		->	eps*/
+/*<DALSI>		->	; <POKYN> <PRVNI>*/
+int DALSI (){
+return 0;
+}
+/*<PRIKAZ>	-> 	id := <VYRAZ>*/
+int PRIKAZ (){
+return 0;
+}
+
+/*<DEK>		->	var id : <TYPE> ; <DEKDAL>*/
+/*<DEK>		->	eps*/
+int DEK (){
+return 0;
+}
+
+/*<DEKDAL>	->	id : <TYPE> ;  <DEKDAL>*/
+/*<DEKDAL>	->	eps*/
+int DEKDAL (){
+return 0;
+}
+
+/*<TYPE>		->	real*/
+/*<TYPE>		->	string*/
+/*<TYPE>		->	integer*/
+/*<TYPE>		->	boolean*/
+int TYPE (){
+return 0;
+}
+
+/*<BOOLEAN>	->	true*/
+/*<BOOLEAN>	->	false*/
+int BOOLEANP (){
+return 0;
+}
+
+/*<VYPIS>		->	id <DVYPIS>*/
+int VYPIS (){
+return 0;
+}
+
+/*<DVYPIS>	->	, <VYPIS>*/
+/*<DVYPIS>	->	eps*/
+int DVYPIS (){
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
--- parsektest/parser.h
+++ parsektest/parser.h
@@ -1,1 +1,22 @@
 int parser();
+ int START ();
+int FUNC ();
+int FORWAR ();
+int ARG ();
+int ARGDAL ();
+int CYKLUS ();
+int KDYZ ();
+int ELSEP ();
+int SEKVENCE ();
+int POKYN ();
+int SLOZ ();
+int PRVNI ();
+int DALSI ();
+int PRIKAZ ();
+int DEK ();
+int DEKDAL ();
+int TYPE ();
+int BOOLEANP ();
+int VYPIS ();
+int DVYPIS ();
+
